4c0c590e2bfd5b9dddf8bde3065c1b0a8a5ee85e
AlexandreCANIVET <canivet.alexandre@yahoo.fr>
2022-07-06 17:16:31 +0200

    TP Color Generator

---
+++ index.js
@@ -0,0 +1,11 @@
+function getColor() {
+  let r = Math.floor(Math.random() * 255);
+  let g = Math.floor(Math.random() * 255);
+  let b = Math.floor(Math.random() * 255);
+
+  const color = `rgb(${r}, ${g}, ${b})`;
+  document.body.style.background = color;
+  document.body.innerHTML = `<span>${color}</span>`;
+}
+
+setInterval(getColor, 1000);
